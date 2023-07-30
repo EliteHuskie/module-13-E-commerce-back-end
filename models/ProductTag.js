f8fe76630203aab0d45ab569e03d903623f0ffd0
EliteHuskie <stratton_10@hotmail.com>
2023-07-30 12:08:59 -0300

    Adding logic for productTag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,36 @@
+// Import models
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
+// Set up fields and rules for ProductTag model
 ProductTag.init(
   {
-    // define columns
+  // Define columns for ProductTag model
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
@@ -17,4 +41,5 @@ ProductTag.init(
   }
 );
 
+// Module exports for ProductTag
 module.exports = ProductTag;
