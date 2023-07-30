403637e0b9c4b4bc395313504d6c387fd1f5dcb1
EliteHuskie <stratton_10@hotmail.com>
2023-07-30 11:51:59 -0300

    Adding server code for application

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+// Import Express JS + Routes
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+// Import sequelize connection
+const sequelize = require('./config/connection');
 
+// App and Port used for application
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,7 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// Sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+  });
 });
